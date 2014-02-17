01e438b23bb31d9761535708c6d2a0302e01580d
Vik <vak.love@gmail.com>
2014-02-17 18:07:38 -0500

    add contact us form

--- BSDemoMVC/Controllers/HomeController.cs
+++ BSDemoMVC/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BSDemoMVC.Models;
+using System.Net.Mail;
+using System.Net;
 
 namespace BSDemoMVC.Controllers
 {
@@ -24,9 +27,47 @@ namespace BSDemoMVC.Controllers
         {
             ViewBag.Message = "";
 
-            return View();
+            return View(new Contactus());
         }
 
+        [HttpPost]
+        public ActionResult Contact(Contactus contactform)
+        {
+            if (ModelState.IsValid)
+            {
+                SendEmail(contactform.Name,contactform.Email,contactform.Phone,contactform.Message);
+                return RedirectToAction("ContactConfirm","Home");
+
+            }
+            return View();
+ 
+        }
+
+        private void SendEmail(string Name,string Email,string Phone ,string Message)
+        {
+            MailMessage msg = new MailMessage();
+            msg.To.Add(new MailAddress(Email));
+            msg.From = new MailAddress("dakbharat@gmail.com", "Contact us");
+            msg.CC.Add(new MailAddress("dakbharat@gmail.com"));
+            msg.Subject = "Contact us inquiry .";
+            string body = "Name: " + Name + "n"
+                        + "Email: " + Email + "n"                        
+                        + "Phone: " + Phone + "n"
+                        + "Message: " + Message + "nn";
+            msg.Body = body;
+            msg.IsBodyHtml = false;
+            SmtpClient smtpclient = new SmtpClient("smtp.gmail.com",587);
+            smtpclient.Credentials = new NetworkCredential("dakbharat@gmail.com", "mypwd");
+            smtpclient.Send(msg);
+            msg.Dispose();
+
+ 
+        }
+        public ActionResult ContactConfirm()
+        {
+            return View();
+        }
+ 
         public ActionResult Quality()
         {
             return View();
---
+++ BSDemoMVC/Models/Contactus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BSDemoMVC.Models
+{
+    public class Contactus
+    {
+     
+        [Required]
+        public string Name { get; set;}
+        [Required]
+        
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage="Enter a vaild Email.")]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+    }
+}
